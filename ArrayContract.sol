c3500546d41d36c8d64001cbf602da2086b558b4
Godfrey Jonas <41231839+jonasgodfrey@users.noreply.github.com>
2023-09-26 20:37:44 -0400

    Update ArrayContract.sol

--- ArrayContract.sol
+++ ArrayContract.sol
@@ -1,36 +1,82 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.18;
+pragma solidity ^0.8.0;
 
+// Library for sorting integers
+library SortLib {
+    function bubbleSort(uint[] memory data) external pure returns(uint[] memory){
+        uint[] memory sortedArr = data;
+        uint n = data.length;
+        bool swapped;
+        do {
+            swapped = false;
+            for (uint i = 0; i < n - 1; i++) {
+                if (sortedArr[i] > sortedArr[i + 1]) {
+                    (sortedArr[i], sortedArr[i + 1]) = (sortedArr[i + 1], sortedArr[i]);
+                    swapped = true;
+                }
+            }
+        } while (swapped);
 
-    library ArrayLib {
-
-        function push(uint[] storage myArray, uint _value) public {
-            myArray.push(_value);
-        }  
-
-      
-    } 
-
-contract ArrayContract{
-    
-    using ArrayLib for uint;    
-    uint[]  myArray;
-    
-    //To add elements to the array
-    function addElement(uint newElement) public {
-        myArray.push(newElement);
-    }  
-
-    //Function to view the array element
-     function viewArray() public view returns (uint[] memory) {
-        return myArray;
+        return sortedArr;
     }
 
-    //To Sort the Array - Got stuck
+    function countUnique(uint[] memory array) internal pure returns (uint) {
+        uint arrayLength = array.length;
+        if (arrayLength <= 1) {
+            return arrayLength;
+        }
 
-    
-    //To remove duplicate element in the array - Got stuck
-    
-   
+        uint uniqueCount = 0;
+        bool[] memory isUnique = new bool[](arrayLength);
 
+        for (uint i = 0; i < arrayLength; i++) {
+            if (!isUnique[i]) {
+                uniqueCount++;
+                for (uint j = i + 1; j < arrayLength; j++) {
+                    if (array[i] == array[j]) {
+                        isUnique[j] = true;
+                    }
+                }
+            }
+        }
+
+        return uniqueCount;
+    }
+
+    function getUniqueElements(uint[] memory array) external pure  returns (uint[] memory) {
+        uint uniqueCount = countUnique(array);
+        uint[] memory uniqueArray = new uint[](uniqueCount);
+        uint currentIndex = 0;
+
+        for (uint i = 0; i < array.length; i++) {
+            if (!contains(uniqueArray, array[i])) {
+                uniqueArray[currentIndex] = array[i];
+                currentIndex++;
+            }
+        }
+
+        return uniqueArray;
+    }
+
+    function contains(uint[] memory array, uint element) internal pure returns (bool) {
+        for (uint i = 0; i < array.length; i++) {
+            if (array[i] == element) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+// Smart contract using the sorting library
+contract SortContract {
+
+    function sort(uint[] memory arr) external pure returns(uint[] memory) {
+       return SortLib.bubbleSort(arr);
+    }
+
+    function getUniqueElements(uint[] memory arr) external pure returns (uint[] memory) {
+        return SortLib.getUniqueElements(arr);
+    }
+}
+ 
